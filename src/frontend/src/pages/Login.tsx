6716828b7161e6db10bf217d15270d1f449073bc
Wilson Wei <2004wwei@gmail.com>
2024-10-12 17:46:11 -0400

    Update Login.tsx

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,19 +1,20 @@
 import React from "react";
+
 const Login: React.FC = () => {
   return (
     <div>
       <div className="bg-black shadow-sm">
-        <a href="/" className="flex items-center p-3">
+        <a href="/" className="flex items-center p-2">
           <img
             src="/logo_white.svg"
             alt="Local.ly Logo"
-            className="h-10 mb-3 w-auto"
+            className="h-10 mb-3 w-auto ml-3"
           />
         </a>
       </div>
       <div className="relative flex items-center justify-center h-screen">
-        <span className="absolute text-black text-3xl mb-[18rem]">Sign in</span>
-        <img src="/gem.svg" className="absolute h-12 w-35 mb-[10rem]"></img>
+        <h1 className="absolute text-black text-3xl mb-[15rem]">Sign in</h1>
+        <img src="/gem.svg" className="absolute h-12 w-35 mb-[9rem]"></img>
         <button className="flex items-center bg-white text-gray-800 border border-gray-300 rounded-lg shadow-md p-3 hover:bg-gray-100 transition duration-200">
           <img
             src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
